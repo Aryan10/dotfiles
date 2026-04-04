92c64db91c0dc319d7028765f59e15d19834dcc4
Nishant040305 <nishant040305@gmail.com>
2026-04-04 19:29:22 +0530

    feat: Add option to for tmux session detach for long running task
    Usage
    
    det <command>
    det --attach <command> attach it to the terminal
    deta (attach the last)
    detk (kill the last)
    detl (list det session uses .cache folder for it)
    
    there are other options like name and etc. added in usage --help

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -1,16 +1,23 @@
-# Color definitions
-readonly _CLR_RESET='\033[0m'
-readonly _CLR_BOLD='\033[1m'
-readonly _CLR_DIM='\033[2m'
-readonly _CLR_INFO='\033[38;5;87m'      # Cyan
-readonly _CLR_SUCCESS='\033[38;5;83m'   # Green
-readonly _CLR_WARN='\033[38;5;221m'     # Yellow
-readonly _CLR_ERROR='\033[38;5;203m'    # Red
-readonly _CLR_ACCENT='\033[38;5;177m'   # Magenta
+# Color definitions (guarded for repeated sourcing)
+if [[ -z "${_CLR_RESET:-}" ]]; then
+    readonly _CLR_RESET='\033[0m'
+    readonly _CLR_BOLD='\033[1m'
+    readonly _CLR_DIM='\033[2m'
+    readonly _CLR_INFO='\033[38;5;87m'      # Cyan
+    readonly _CLR_SUCCESS='\033[38;5;83m'   # Green
+    readonly _CLR_WARN='\033[38;5;221m'     # Yellow
+    readonly _CLR_ERROR='\033[38;5;203m'    # Red
+    readonly _CLR_ACCENT='\033[38;5;177m'   # Magenta
+fi
 
 source "$HOME/.dotfiles/proxy/proxy.zsh"
 
+# Detach a long-running command into its own tmux session.
+det() {
+    "$HOME/.dotfiles/bin/det" "$@"
+}
+
 # Reboot directly into Windows
 winboot() {
-    sudo grub2-reboot 'Windows Boot Manager (on /dev/nvme0n1p1)' && reboot
+    sudo grub2-reboot "Windows Boot Manager (on /dev/nvme0n1p1)" && reboot
 }
